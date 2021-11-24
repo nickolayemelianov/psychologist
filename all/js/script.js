1b7c04a424a7861036c8c425dc2383a9fc9c1e6e
nickolayemelianov <94912965+nickolayemelianov@users.noreply.github.com>
2021-11-24 15:35:08 +0300

    Add files via upload

---
+++ all/js/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.slider').slick({
+        arrows: true,
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        speed: 700
+    })
+});
+
+$(document).ready(function(){
+    $('.media__slider').slick({
+        arrows: true,
+        dots: true,
+        adaptiveHeight: true,
+        slidesToShow: 1,
+        speed: 700
+    })
+});
+
